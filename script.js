fef2b774432608b6163de761cc56f1830d2d9789
nanda158 <117355312+nanda158@users.noreply.github.com>
2024-08-26 11:58:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {aleatorio} from './aleatorio.js';
+import {perguntas} from './aleatorio.js';
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -6,80 +8,6 @@ const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
 const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
-
-const perguntas = [
-    {
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
-                ]
-            },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
-                ]
-            }
-        ]
-    },
-    {
-      enunciado:
-        "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
-      alternativas: [
-        {
-          texto:
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-          afirmacao: ["Sim, ela pode ajudar em alguns apectos"
-        ]
-        },
-        {
-          texto:
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-          afirmacao: ["Realmente essa parte pode prejudicar muitos ciadoẽs"
-        ]
-        },
-      ],
-    },
-    {
-      enunciado:
-        "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-      alternativas: [
-        {
-          texto:
-            "Critransition: background-color 0.3s;ar uma imagem utilizando uma plataforma de design como o Paint.",
-          afirmacao: ["criação de imagem com o paint",
-        ]
-        },
-        {
-          texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-          afirmacao: ["imagem gerada por IA",
-        ]
-        },
-      ],
-    },
-    {
-      enunciado:
-        "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-      alternativas: [
-        {
-          texto:
-            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-          afirmacao: ["isso não é verdade, vocẽ só especificou oque vocẽ queria",
-        ]
-        },
-        {
-          texto:
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-          afirmacao: ["sim, é precio tomar cuidado",
-        ]
-        },
-      ],
-    },
-  ];
 
   let atual = 0;
   let perguntaAtual;
@@ -115,7 +43,7 @@ function mostraAlternativas(){
 
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historia += afirmacoes + "";
     atual++;
     mostraPergunta();
